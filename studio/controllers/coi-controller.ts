ea9744473f6649c80fa08133a9fdccd6c3ca256c
Arman <eccs0103@gmail.com>
2026-05-29 01:19:30 +0400

    3.0.0

--- studio/controllers/coi-controller.ts
+++ studio/controllers/coi-controller.ts
@@ -7,6 +7,8 @@ const { baseURI } = document;
 
 //#region CORS isolation controller
 export class CorsIsolationController extends Controller {
+	static #MAX_32_BIT_INT: number = 2147483647;
+
 	async run(): Promise<void> {
 		if (crossOriginIsolated) return;
 		const registration = await navigator.serviceWorker.register(new URL("../coi-worker.js", baseURI), { type: "module" });
@@ -15,7 +17,7 @@ export class CorsIsolationController extends Controller {
 			if (registration.active !== null) resolve();
 		});
 		location.reload();
-		await Promise.asTimeout(Infinity);
+		await Promise.asTimeout(CorsIsolationController.#MAX_32_BIT_INT);
 	}
 
 	async catch(error: Error): Promise<void> {
