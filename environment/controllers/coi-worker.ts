e9c14f6cc7126caaf28c64d1c013f3fe78f9f904
Arman <eccs0103@gmail.com>
2026-06-02 02:32:47 +0400

    3.0.0

--- environment/controllers/coi-worker.ts
+++ environment/controllers/coi-worker.ts
@@ -1,31 +1,46 @@
 "use strict";
 
 import "adaptive-extender/worker";
+import { Controller } from "adaptive-extender/worker";
 
-interface COIScope {
+//#region COI service worker
+interface ServiceWorkerGlobalScope {
 	readonly clients: { claim(): Promise<void>; };
 	skipWaiting(): Promise<void>;
 	addEventListener(type: "install" | "activate", listener: (event: { waitUntil(f: PromiseLike<unknown>): void; }) => void): void;
 	addEventListener(type: "fetch", listener: (event: { request: Request; respondWith(r: PromiseLike<Response> | Response): void; }) => void): void;
 }
 
-async function buildIsolatedResponse(request: Request): Promise<Response> {
-	let response: Response;
-	try {
-		response = await fetch(request);
-	} catch {
-		return Response.error();
+class CorsIsolationWorker extends Controller {
+	async #tryFetch(request: Request): Promise<Response> {
+		try {
+			return await fetch(request);
+		} catch {
+			return Response.error();
+		}
 	}
-	if (response.status === 0) return response;
-	const { body, status, statusText } = response;
-	const headers = new Headers(response.headers);
-	headers.set("Cross-Origin-Opener-Policy", "same-origin");
-	headers.set("Cross-Origin-Embedder-Policy", "require-corp");
-	return new Response(body, { status, statusText, headers });
-}
 
-const scope: COIScope = self as unknown as COIScope;
-scope.addEventListener("install", event => event.waitUntil(scope.skipWaiting()));
-scope.addEventListener("activate", event => event.waitUntil(scope.clients.claim()));
-scope.addEventListener("fetch", event => event.respondWith(buildIsolatedResponse(event.request)));
+	async #buildIsolatedResponse(request: Request): Promise<Response> {
+		const response = await this.#tryFetch(request);
+		if (response.status === 0) return response;
+		const { body, status, statusText } = response;
+		const headers = new Headers(response.headers);
+		headers.set("Cross-Origin-Opener-Policy", "same-origin");
+		headers.set("Cross-Origin-Embedder-Policy", "require-corp");
+		return new Response(body, { status, statusText, headers });
+	}
+
+	async run(): Promise<void> {
+		const scope = self as unknown as ServiceWorkerGlobalScope;
+		scope.addEventListener("install", event => event.waitUntil(scope.skipWaiting()));
+		scope.addEventListener("activate", event => event.waitUntil(scope.clients.claim()));
+		scope.addEventListener("fetch", event => event.respondWith(this.#buildIsolatedResponse(event.request)));
+	}
+
+	async catch(error: Error): Promise<void> {
+		throw error;
+	}
+}
 //#endregion
+
+await CorsIsolationWorker.launch();
